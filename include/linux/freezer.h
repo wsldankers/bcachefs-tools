bf7924f5526707e01213cad546794571764bc177
Kent Overstreet <kent.overstreet@gmail.com>
2022-03-10 15:38:02 -0500

    Add freezable_schedule() to linux kernel shim
    Also fix the freezable_schedule_timeout() macro.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- include/linux/freezer.h
+++ include/linux/freezer.h
@@ -4,6 +4,7 @@
 #define try_to_freeze()
 #define set_freezable()
 #define freezing(task)		false
-#define freezable_schedule_timeout(_t) schedule_timeout(_t);
+#define freezable_schedule()	schedule()
+#define freezable_schedule_timeout(_t) schedule_timeout(_t)
 
 #endif /* __TOOLS_LINUX_FREEZER_H */
